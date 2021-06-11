a6e256a1fce8ee591a3672668fac3a1de1b1a412
Nikolai <nik.moissejev@gmail.com>
2021-06-11 13:58:05 +0300

    homework is finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 "use strict";
 
-let numberOfFilms;
-numberOfFilms = prompt("How many movies have you watched?", "");
+const numberOfFilms = +prompt("How many movies have you watched?", "");
 
-console.log(numberOfFilms);
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+const firstMovie = prompt("One of the last movies you have watched?", ""),
+     firstAnswer = prompt("Give it a raiting", ""),
+     secondMovie = prompt("One of the last movies you have watched?", ""),
+     secondAnswer = prompt("Give it a raiting", "");
+
+personalMovieDB.movies[firstMovie] = firstAnswer;
+personalMovieDB.movies[secondMovie] = secondAnswer;
+
+console.log(personalMovieDB);
