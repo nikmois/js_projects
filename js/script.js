18041cca62d14e1f4e0b18f5bcefa0f2527c23de
Nikolai <nik.moissejev@gmail.com>
2021-06-14 12:58:38 +0300

    made second homework

--- js/script.js
+++ js/script.js
@@ -12,12 +12,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const firstMovie = prompt("One of the last movies you have watched?", ""),
-     firstAnswer = prompt("Give it a raiting", ""),
-     secondMovie = prompt("One of the last movies you have watched?", ""),
-     secondAnswer = prompt("Give it a raiting", "");
 
-personalMovieDB.movies[firstMovie] = firstAnswer;
-personalMovieDB.movies[secondMovie] = secondAnswer;
+
+
+for (let i = 1; i <= 2; i++) {
+    const firstMovie = prompt("One of the last movies you have watched?", "");
+    const firstAnswer = prompt("Give it a raiting", "");
+    if (firstMovie != null && firstAnswer != null && firstMovie !='' && firstAnswer !='' && firstMovie.length < 50 && firstAnswer.length <50){
+        console.log("done");
+        personalMovieDB.movies[firstMovie] = firstAnswer;
+    } else{
+        console.log("error");
+        i--;
+    }
+    
+}
+
+
+if (personalMovieDB.count < 10){
+    console.log("too less");
+} else if (10 <= personalMovieDB.count < 30){
+    console.log("quite good");
+} else if (personalMovieDB.count >= 30){
+    console.log("wow, u are real movie lover");
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDB);
