24843e502dee086d42c1a3c8457621a81d7ffd6c
Nikolai <nik.moissejev@gmail.com>
2021-06-14 11:40:14 +0300

    added loops file

---
+++ js/loops.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6){
+        // break; // stopping cycle
+        continue; // letting cycle to miss one number and continues
+    }
+    console.log(i);
+    
+}
