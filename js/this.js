9557f8c15686fca2fd636f5188446b8b4e2a62ec
Nikolai <nik.moissejev@gmail.com>
2021-08-14 16:38:02 +0300

    working with 'this', using call context

---
+++ js/this.js
@@ -0,0 +1,82 @@
+'use strict';
+
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+
+//     console.log(sum());
+// }
+
+// showThis(4, 5);
+
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     };
+// }
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+
+//1) usual function: this = undefined
+//2) Контекст у методов объекта - сам объект
+//3) this in constructors and classes - new copy of object
+//4) this: call, apply, bind
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    this.style.backgroundColor = 'red';
+    this.style.width = 100 + 'px';
+    this.style.height = 100 + 'px';
+
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+const double = a => a * 2;  //short syntax
+
+
