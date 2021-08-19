dc67d85f41da663ad391b9f7d7930a5d7ac33a80
Nikolai <nik.moissejev@gmail.com>
2021-08-19 13:41:28 +0300

    using REST operator, adding classes for tabs classList, setting default class for REST operator

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -164,7 +164,7 @@ window.addEventListener('DOMContentLoaded', () => {
         render() {
             const element = document.createElement('div');
             if (this.classes.length === 0) {
-                this.element = 'menu__item'
+                this.element = 'menu__item';
                 element.classList.add(this.element);
             } else {
                 this.classes.forEach(className => element.classList.add(className));
