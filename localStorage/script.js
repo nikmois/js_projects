41ef5a2e679eb30dfdecf1024568ef4aa227ce33
Nikolai <nik.moissejev@gmail.com>
2021-09-01 18:21:07 +0300

    working with localStorage

---
+++ localStorage/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// localStorage.setItem('number', 5); //adds element
+
+// localStorage.removeItem('number'); //deletes element
+
+// localStorage.clear(); //clears all local storage
+
+// console.log(localStorage.getItem('number')); //gets item
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = 'white';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+const persone = {
+    name: 'Alex',
+    age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
